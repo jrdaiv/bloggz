27f88f653d2d7bc3a8b5edd920e61270f8f386a9
Anthony Hernandez <jrdaiv@icloud.com>
2025-07-21 21:39:07 -0700

    last commit hopefully

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -9,16 +9,11 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./config/db"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const posts_1 = __importDefault(require("./routes/posts"));
-const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 (0, db_1.default)();
 app.use((0, cors_1.default)({ origin: "http://localhost:5173" }));
 app.use(express_1.default.json());
-app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'frontend', 'dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
 app.use("/api/auth", auth_1.default);
 app.use("/api/posts", posts_1.default);
 app.get("/", (req, res) => {
